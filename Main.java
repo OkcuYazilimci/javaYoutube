b40f61b0b7d1116af70bff97510fc1915a840f52
umut <113436899+CoderVega@users.noreply.github.com>
2022-11-07 12:33:38 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-// Bunu forla yapmamız lazım
-public class Main {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-    }
-}
